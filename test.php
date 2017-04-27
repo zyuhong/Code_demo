dffbaff42b3dcef4f7901d217270272694bb911d
unknown <>
2017-04-27 12:36:04 +0800

    test

---
+++ test.php
@@ -0,0 +1,20 @@
+<?php
+   $cf_it2 = [
+            [
+            'recall' => 0.01764, 
+            'precission' => 0.0525
+            ],
+            [
+            'recall' => 0.0168, 
+            'precission' => 0.056
+            ],
+            [
+            'recall' => 0.0151, 
+            'precission' => 0.042
+            ],
+            [
+            'recall' => 0.0149, 
+            'precission' => 0.037
+            ],
+          ];
+?>
